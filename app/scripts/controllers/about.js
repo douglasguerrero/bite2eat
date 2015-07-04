96320a9699a590c698425c18c49e7e31775ec9d2
Camilo Aguilar <camiloaguilar1@yahoo.com>
2015-07-04 14:41:43 -0600

    updating

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name bite2eatApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the bite2eatApp
+ */
+angular.module('bite2eatApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name bite2eatApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the bite2eatApp
+ */
+angular.module('bite2eatApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
