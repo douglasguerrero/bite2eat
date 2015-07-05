600d160c004b569b477ba15910d0b348d7e2bfdf
douglasguerrero <dguerrero5@hotmail.com>
2015-07-04 19:50:24 -0600

    added menuItems to the search functionality.

--- app/scripts/controllers/about.js
+++ app/scripts/controllers/about.js
@@ -8,24 +8,10 @@
  * Controller of the bite2eatApp
  */
 angular.module('bite2eatApp')
-  .controller('AboutCtrl', function ($scope) {
-  	Parse.initialize("WSA1398Zaliy9pauhvHEg5uzfvrqwH1bXdmwRW7e", "4EiQQjH6Hm6Lfnl172GMNDCorWyghusvACWEhg9I");
-
-			$scope.searchInput = function() {
-					var Restaurant = Parse.Object.extend("Restaurant");
-					var query = new Parse.Query(Restaurant);
-					query.containedIn("foodTypes", [$scope.searchValue]);
-					query.find({
-						success: function(results) {
-							alert("Successfully retrieved " + results.length + " scores.");
-							for (var i = 0; i < results.length; i++) {
-								var object = results[i];
-								alert(object.get('foodTypes'));
-							}
-						},
-						error: function(error) {
-							alert("Error: " + error.code + " " + error.message);
-						}
-					});
-				};
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
   });
--- app/scripts/controllers/search.js
+++ app/scripts/controllers/search.js
@@ -9,21 +9,36 @@
  */
 angular.module('bite2eatApp')
 	.controller('SearchCtrl', function($scope) {
-			Parse.initialize("WSA1398Zaliy9pauhvHEg5uzfvrqwH1bXdmwRW7e", "4EiQQjH6Hm6Lfnl172GMNDCorWyghusvACWEhg9I");
+		Parse.initialize("WSA1398Zaliy9pauhvHEg5uzfvrqwH1bXdmwRW7e", "4EiQQjH6Hm6Lfnl172GMNDCorWyghusvACWEhg9I");
 
-			$scope.searchInput = function() {
-					var Restaurant = Parse.Object.extend("Restaurant");
-					var query = new Parse.Query(Restaurant);
-					query.containedIn("foodTypes", [$scope.searchValue.toLowerCase()]);
-					query.find({
-						success: function(results) {
-							$scope.$apply(function() {
-								$scope.results = results;
-							});
-						},
-						error: function(error) {
-							alert("Error: " + error.code + " " + error.message);
-						}
+		$scope.searchInput = function() {
+			var Restaurant = Parse.Object.extend("Restaurant");
+			var query = new Parse.Query(Restaurant);
+			query.containedIn("foodTypes", [$scope.searchValue.toLowerCase()]);
+			query.find({
+				success: function(results) {
+					$scope.$apply(function() {
+						$scope.results = results;
 					});
-				};
+				},
+				error: function(error) {
+					alert("Error: " + error.code + " " + error.message);
+				}
 			});
+		};
+
+		$scope.menuItem = function(restaurantId) {
+			var MenuItem = Parse.Object.extend("MenuItem");
+			var query = new Parse.Query(MenuItem);
+			query.get(restaurantId, {
+				success: function(menuItems) {
+					$scope.$apply(function() {
+						$scope.menuItems = menuItems;
+					});
+				},
+				error: function(object, error) {
+					alert("Error: " + error.code + " " + error.message);
+				}
+			});
+		};
+	});
