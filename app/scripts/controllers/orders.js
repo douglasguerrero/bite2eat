e1855ffbc3ea328360a42f7a5ab4ba8f0040800f
Camilo Aguilar <camiloaguilar1@yahoo.com>
2015-07-05 11:16:31 -0600

    adding order detail

--- app/scripts/controllers/orders.js
+++ app/scripts/controllers/orders.js
@@ -8,7 +8,7 @@
  * Controller of the bite2eatApp
  */
 angular.module('bite2eatApp')
-  .controller('OrdersCtrl', function ($rootScope,$scope) {
+  .controller('OrdersCtrl', function ($rootScope,$scope, $modal,$log) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
@@ -18,6 +18,27 @@ angular.module('bite2eatApp')
     $rootScope.showHeader = true;
 
     $scope.orders = []
+
+    $scope.open = function (size,order) {
+
+	    var modalInstance = $modal.open({
+	      animation: $scope.animationsEnabled,
+	      templateUrl: '/views/orderdetail.html',
+	      controller: 'OrderdetailCtrl',
+	      size: size,
+	       resolve: {
+	        order: function () {
+	          return order;
+	        }
+	      }
+	    });
+
+	    modalInstance.result.then(function (selectedItem) {
+	      //$scope.selected = selectedItem;
+	    }, function () {
+	      $log.info('Modal dismissed at: ' + new Date());
+	    });
+	 };
 
     $scope.loadOrders = function(){
     	var Orders = Parse.Object.extend("Order");
@@ -31,7 +52,8 @@ angular.module('bite2eatApp')
 				      	objectId: object.id,
 				      	clientName: object.get('clientName'),
 				      	status: object.get('status'),
-				      	total: object.get('total')
+				      	total: object.get('total'),
+				      	createdDate: object.createdAt
 				      });
 	          	});
 		      
